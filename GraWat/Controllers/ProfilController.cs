16218229e7781e9ef010e184c55b733d663bf3d3
msendul <muhammed56566565@gmail.com>
2026-06-04 15:19:42 +0300

    feat: Favoriler sistemi, sayfalama ve dinamik alt kategori menüsü eklendi

--- GraWat/Controllers/ProfilController.cs
+++ GraWat/Controllers/ProfilController.cs
@@ -20,7 +20,7 @@ namespace GraWat.Controllers
 
         // --- FAVORİLERİM VE ÖZEL FAVORİ İÇİ ARAMA METODU ---
         [Authorize]
-        public async Task<IActionResult> Favorilerim(string favoriArama)
+        public async Task<IActionResult> Favorilerim(string favoriArama, int page = 1)
         {
             // 1. Giriş yapan kullanıcının kimliğini alıyoruz
             var userId = _userManager.GetUserId(User);
@@ -40,8 +40,25 @@ namespace GraWat.Controllers
                 ViewBag.FavoriAramaKelimesi = favoriArama; // Arama kutusunda kelime yazılı kalsın diye
             }
 
-            // 4. Verileri asenkron olarak listeye döküp View'a gönderiyoruz
-            var favoriUrunlerListesi = await favoriQuery.Select(f => f.Urun).ToListAsync();
+            // 4. Sayfalama Hesaplamaları
+            int pageSize = 6;
+            int totalItems = await favoriQuery.CountAsync();
+            int totalPages = (int)Math.Ceiling((double)totalItems / pageSize);
+
+            if (page < 1) page = 1;
+            if (totalPages > 0 && page > totalPages) page = totalPages;
+
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalPages = totalPages;
+            ViewBag.TotalItems = totalItems;
+
+            // 5. Verileri asenkron olarak listeye döküp View'a gönderiyoruz
+            var favoriUrunlerListesi = await favoriQuery
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .Select(f => f.Urun)
+                .ToListAsync();
+                
             return View(favoriUrunlerListesi);
         }
 
@@ -65,6 +82,52 @@ namespace GraWat.Controllers
             }
 
             return RedirectToAction(nameof(Favorilerim));
+        }
+
+        // --- AJAX FAVORİ EKLE/ÇIKAR ---
+        [HttpPost]
+        public async Task<IActionResult> ToggleFavorite(int id)
+        {
+            var userId = _userManager.GetUserId(User);
+            if (string.IsNullOrEmpty(userId))
+            {
+                if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+                {
+                    return Json(new { success = false, message = "Favorilere eklemek için lütfen giriş yapınız.", redirectUrl = "/Identity/Account/Login" });
+                }
+                return RedirectToPage("/Account/Login", new { area = "Identity" });
+            }
+
+            var favori = await _context.Favoriler
+                .FirstOrDefaultAsync(f => f.UrunId == id && f.KullaniciId == userId);
+
+            bool isAdded;
+            if (favori != null)
+            {
+                _context.Favoriler.Remove(favori);
+                isAdded = false;
+            }
+            else
+            {
+                var yeniFavori = new Favoriler
+                {
+                    UrunId = id,
+                    KullaniciId = userId
+                };
+                _context.Favoriler.Add(yeniFavori);
+                isAdded = true;
+            }
+
+            await _context.SaveChangesAsync();
+
+            var favoritesCount = await _context.Favoriler.CountAsync(f => f.KullaniciId == userId);
+
+            return Json(new { 
+                success = true, 
+                isAdded = isAdded, 
+                favoritesCount = favoritesCount, 
+                message = isAdded ? "Ürün favorilerinize eklendi!" : "Ürün favorilerinizden çıkarıldı!" 
+            });
         }
     }
 }
